9fe517746706c20e3a6e55f183db9a513cf90c82
Nikita <nsolodkov45@gmail.com>
2024-02-12 19:52:06 +0200

    Added scrolldown update new characters

---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Никита Солодков on 09.02.2024.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
