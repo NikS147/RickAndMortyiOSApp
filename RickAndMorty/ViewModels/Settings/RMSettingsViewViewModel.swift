6c4d53bac33c94940e4fc441a08fa9fd3f814c22
Nikita <nsolodkov45@gmail.com>
2024-02-24 18:11:57 +0200

    Finished developing the SettingsView

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Никита Солодков on 24.02.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by Никита Солодков on 24.02.2024.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RMSettingsView()
+}
