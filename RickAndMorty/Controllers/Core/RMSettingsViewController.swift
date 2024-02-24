5f9c1e96591fd7cd009e4c8c3723738965e63872
Nikita <nsolodkov45@gmail.com>
2024-02-24 18:12:17 +0200

    Finished developing the SettingsView

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -5,17 +5,58 @@
 //  Created by Никита Солодков on 06.02.2024.
 //
 
+import StoreKit
+import SafariServices
 import UIKit
+import SwiftUI
 
 /// Controller to show various app options and settings
 final class RMSettingsViewController: UIViewController {
-
+    
+    private var settingsSwiftUIController: UIHostingController<RMSettingsView>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         title = "Settings"
+        addSwiftUIController()
     }
     
-
+    private func addSwiftUIController() {
+        let settingsSwiftUIController = UIHostingController(rootView: RMSettingsView(viewModel: RMSettingsViewViewModel(cellViewModels: RMSettingsOption.allCases.compactMap {
+            return RMSettingsCellViewModel(type: $0) { [weak self] option in
+                self?.handleTap(option: option)
+            }
+        })))
+        
+        addChild(settingsSwiftUIController)
+        settingsSwiftUIController.didMove(toParent: self)
+        
+        view.addSubview(settingsSwiftUIController.view)
+        settingsSwiftUIController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            settingsSwiftUIController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            settingsSwiftUIController.view.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            settingsSwiftUIController.view.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            settingsSwiftUIController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        self.settingsSwiftUIController = settingsSwiftUIController
+    }
+    
+    private func handleTap(option: RMSettingsOption) {
+        guard Thread.current.isMainThread else { return }
+        
+        if let url = option.targetURL {
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        } else if option == .rateApp {
+            if let windowScene = self.view.window?.windowScene {
+                SKStoreReviewController.requestReview(in: windowScene)
+            }
+            
+        }
+    }
 }
--- RickAndMorty/ViewModels/Settings/RMSettingsCellViewModel.swift
+++ RickAndMorty/ViewModels/Settings/RMSettingsCellViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by Никита Солодков on 24.02.2024.
 //
 
-import Foundation
+import UIKit
+
+struct RMSettingsCellViewModel: Identifiable {
+    let id = UUID()
+    
+    public let type: RMSettingsOption
+    public let onTapHandler: (RMSettingsOption) -> Void
+    
+    // MARK: - Init
+    
+    init(type: RMSettingsOption, onTapHandler: @escaping (RMSettingsOption) -> Void) {
+        self.type = type
+        self.onTapHandler = onTapHandler
+    }
+    
+    // MARK: - Public
+    
+    public var image: UIImage? {
+        return type.iconImage
+    }
+    
+    public var title: String {
+        return type.displayTitle
+    }
+    
+    public var iconContainerColor: UIColor {
+        return type.iconContainerColor
+    }
+}
