0300ea77541b5e765891b5bebc13e39accfc5d47
Nikita <nsolodkov45@gmail.com>
2024-02-28 21:47:06 +0200

    Added no search results view

--- RickAndMorty/Controllers/Other/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/RMSearchViewController.swift
@@ -35,13 +35,15 @@ final class RMSearchViewController: UIViewController {
         }
         let type: `Type`
     }
-    
-    private let config: RMConfig
+    private let viewModel: RMSearchViewViewModel
+    private let searchView: RMSearchView
     
     // MARK: - Init
     
     init(config: RMConfig) {
-        self.config = config
+        let viewModel = RMSearchViewViewModel(config: config)
+        self.viewModel = viewModel
+        self.searchView = RMSearchView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -53,8 +55,24 @@ final class RMSearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = config.type.title
+        title = viewModel.config.type.title
         view.backgroundColor = .systemBackground
+        view.addSubviews(searchView)
+        addConstraints()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Search", style: .done, target: self, action: #selector(didTapExecuteSearch))
+    }
+    
+    @objc private func didTapExecuteSearch() {
+//        viewModel.executeSearch()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            searchView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
 }
--- RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by Никита Солодков on 28.02.2024.
 //
 
-import Foundation
+import UIKit
 
 struct RMNoSearchResultsViewViewModel {
-    
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
 }
